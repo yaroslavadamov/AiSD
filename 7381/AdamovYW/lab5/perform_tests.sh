d3c57fb9a81d3aca8bdfadd861f9e11b20fcdce5
Yaroslav Adamov <32567078+yaroslavadamov@users.noreply.github.com>
2018-12-04 22:43:43 +0300

    Update perform_tests.sh

--- 7381/AdamovYW/lab5/perform_tests.sh
+++ 7381/AdamovYW/lab5/perform_tests.sh
@@ -1,2 +1,14 @@
 echo "Test 1: ceafbdg"
 ./program < Tests/pt1.txt
+
+echo "Test 2: abcdef"
+./program < Tests/pt2.txt
+
+echo "Test 3: hfksucnzabmqo"
+./program < Tests/pt3.txt
+
+echo "Test 4: abdfcadqqjb"
+./program < Tests/pt4.txt
+
+echo "Test 5: a-d.,gb vx"
+./program < Tests/pt5.txt
